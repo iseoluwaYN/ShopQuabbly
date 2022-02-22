4697291cb13e044346282e3f3d05f3fbdf717060
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-02-22 20:02:59 +0100

    create stock

--- src/main/java/com/quabbly/shopQuabbly/data/services/StockService.java
+++ src/main/java/com/quabbly/shopQuabbly/data/services/StockService.java
@@ -2,11 +2,15 @@ package com.quabbly.shopQuabbly.data.services;
 
 import com.quabbly.shopQuabbly.data.dto.StockDTO;
 import com.quabbly.shopQuabbly.data.model.Stock;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public interface StockService {
-    void create(Stock stock);
+    Stock create(StockDTO stock);
     void update(StockDTO stockDTO);
-    Stock getStock(String id);
-    Stock findAllStocksById();
-    void deleteById(String id);
+    Stock getStock(Long id);
+    List<Stock> findAllStocksById();
+    void deleteById(Long id);
 }
--- src/main/java/com/quabbly/shopQuabbly/data/services/StockServiceImpl.java
+++ src/main/java/com/quabbly/shopQuabbly/data/services/StockServiceImpl.java
@@ -1,14 +1,31 @@
 package com.quabbly.shopQuabbly.data.services;
 
 import com.quabbly.shopQuabbly.data.dto.StockDTO;
+import com.quabbly.shopQuabbly.data.exception.StockException;
 import com.quabbly.shopQuabbly.data.model.Stock;
+import com.quabbly.shopQuabbly.data.repository.StockRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class StockServiceImpl implements StockService{
-
+    @Autowired
+    StockRepository stockRepository;
 
     @Override
-    public void create(Stock stock) {
-
+    public Stock create(StockDTO stockDTO) {
+        if(stockDTO != null){
+        validateStockVariables(stockDTO);
+        ModelMapper modelMapper = new ModelMapper();
+        Stock stock = modelMapper.map(stockDTO, Stock.class);
+        return stockRepository.save(stock);
+        }
+        else {
+            throw new StockException("Stock cannot be null");
+        }
     }
 
     @Override
@@ -17,17 +34,30 @@ public class StockServiceImpl implements StockService{
     }
 
     @Override
-    public Stock getStock(String id) {
+    public Stock getStock(Long id) {
         return null;
     }
 
     @Override
-    public Stock findAllStocksById() {
+    public List<Stock> findAllStocksById() {
         return null;
     }
 
     @Override
-    public void deleteById(String id) {
+    public void deleteById(Long id) {
 
     }
+
+    private void validateStockVariables(StockDTO stockDTO) {
+        if (stockDTO.getName() == null || stockDTO.getName().isBlank()
+                || stockDTO.getName().isEmpty()) {
+            throw new StockException("Stock must not have empty name");
+        }
+        if (stockDTO.getCurrentPrice() == 0 || stockDTO.getCurrentPrice() < 0) {
+            throw new StockException("Stock price cannot be zero or have negative value");
+        }
+
+    }
+
+
 }
