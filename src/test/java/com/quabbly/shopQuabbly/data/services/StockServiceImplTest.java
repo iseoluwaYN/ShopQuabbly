19c22c2f0622fdab6b41879b132b49c1210be95c
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-02-23 13:50:19 +0100

    test

--- src/test/java/com/quabbly/shopQuabbly/data/services/StockServiceImplTest.java
+++ src/test/java/com/quabbly/shopQuabbly/data/services/StockServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.quabbly.shopQuabbly.data.services;
 
 import com.quabbly.shopQuabbly.data.dto.StockDTO;
+import com.quabbly.shopQuabbly.data.exception.StockException;
 import com.quabbly.shopQuabbly.data.model.Stock;
 import com.quabbly.shopQuabbly.data.repository.StockRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,7 +9,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class StockServiceImplTest {
 
@@ -19,7 +22,7 @@ class StockServiceImplTest {
 
     @BeforeEach
     void setUp() {
-//        stockRepository.deleteAll();
+        stockRepository = null;
     }
 
     @Test
@@ -31,6 +34,40 @@ class StockServiceImplTest {
         assertNotNull(stock);
     }
 
-//    @Test
-//    void
+    @Test
+    void stockDetailsCannotBeEmpty(){
+        StockDTO stock = new StockDTO();
+        assertThrows(StockException.class, () -> stockServiceImpl.create(stock));;
+    }
+
+    @Test
+    void stockCanBeFoundById(){
+//        create stock
+        StockDTO stock = new StockDTO();
+        stock.setName("headset");
+        stock.setCurrentPrice(12000.00);
+
+        Stock createdStock = stockServiceImpl.create(stock);
+        assertNotNull(stock);
+
+//        find stock
+        assertNotNull(createdStock.getId());
+    }
+
+    @Test
+    void testThatStockCanBeUpdated(){
+        StockDTO stock = new StockDTO();
+        stock.setName("headset");
+        stock.setCurrentPrice(12000.00);
+
+        Stock createdStock = stockServiceImpl.create(stock);
+        assertNotNull(createdStock.getId());
+
+        StockDTO updateStock = new StockDTO();
+        updateStock.setCurrentPrice(12500.00);
+        updateStock.setName("headset");
+        Stock updatedStock = stockServiceImpl.update(createdStock.getId(), updateStock);
+        assertThat( 12500.0).isEqualTo(updatedStock.getCurrentPrice());
+    }
+
 }
